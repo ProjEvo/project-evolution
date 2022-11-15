a30182dd27586206515b3c2119d4a531acf44c44
Timothy Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2022-11-14 18:24:50 -0600

    Change creature part storage to `HashMap` (#11)

--- src/creature.rs
+++ src/creature.rs
@@ -1,21 +1,62 @@
 //! Contains the [Creature] struct and all related components of it
 
+use std::collections::HashMap;
+
 use uuid::{self, Uuid};
 
 /// A creature, made up of [Node]s and [Muscle]s. Contains a unique id for reference.
 pub struct Creature {
-    pub id: Uuid,
-    pub nodes: Vec<Node>,
-    pub muscles: Vec<Muscle>,
+    id: Uuid,
+    nodes: HashMap<Uuid, Node>,
+    muscles: HashMap<Uuid, Muscle>,
 }
 
 impl Creature {
-    /// Creates a new creature from a vector of [Node]s and a vector of [Muscle]s
-    pub fn new(nodes: Vec<Node>, muscles: Vec<Muscle>) -> Creature {
+    /// Creates a new empty creature
+    pub fn new() -> Creature {
         Creature {
             id: Uuid::new_v4(),
-            nodes,
-            muscles,
+            nodes: HashMap::new(),
+            muscles: HashMap::new(),
+        }
+    }
+
+    /// Returns the unique id of the [Creature]
+    pub fn id(&self) -> &Uuid {
+        &self.id
+    }
+
+    /// Returns the nodes the [Creature]
+    pub fn nodes(&self) -> &HashMap<Uuid, Node> {
+        &self.nodes
+    }
+
+    /// Returns the unique id of the [Creature]
+    pub fn muscles(&self) -> &HashMap<Uuid, Muscle> {
+        &self.muscles
+    }
+
+    /// Adds a [Node] to the [Creature]
+    pub fn add_node(&mut self, node: Node) {
+        self.nodes.insert(node.id, node);
+    }
+
+    /// Adds a set of [Node]s to the [Creature]
+    pub fn add_nodes(&mut self, nodes: Vec<Node>) {
+        for node in nodes {
+            self.add_node(node);
+        }
+    }
+
+    /// Adds a [Muscle] to the [Creature]
+    pub fn add_muscle(&mut self, muscle: Muscle) {
+        self.muscles.insert(muscle.id, muscle);
+    }
+
+    /// Adds a set of [Node]s to the [Creature]
+    pub fn add_muscles(&mut self, muscles: Vec<Muscle>) {
+        for muscle in muscles {
+            self.add_muscle(muscle);
         }
     }
 }
@@ -78,6 +119,8 @@ mod tests {
 
     #[test]
     pub fn create_creature() {
+        let mut c = Creature::new();
+
         let nodes = Vec::from([
             Node::new(Position::new(1, 2), 3),
             Node::new(Position::new(2, 1), 3),
@@ -89,11 +132,16 @@ mod tests {
         let id3 = nodes.get(2).unwrap().id;
 
         let muscles = Vec::from([Muscle::new(id1, id2), Muscle::new(id2, id3)]);
+        let id4 = muscles.get(0).unwrap().id;
 
-        let c = Creature::new(nodes, muscles);
+        c.add_nodes(nodes);
+        c.add_muscles(muscles);
 
-        assert_eq!(c.nodes.get(0).unwrap().position.x, 1.0);
-        assert_eq!(c.nodes.get(2).unwrap().position.x, 5.0);
-        assert_eq!(c.muscles.get(1).unwrap().to_id, c.nodes.get(2).unwrap().id);
+        assert_eq!(c.nodes().get(&id1).unwrap().position.x, 1.0);
+        assert_eq!(c.nodes().get(&id3).unwrap().position.x, 5.0);
+        assert_eq!(
+            c.muscles().get(&id4).unwrap().to_id,
+            c.nodes.get(&id2).unwrap().id
+        );
     }
 }
