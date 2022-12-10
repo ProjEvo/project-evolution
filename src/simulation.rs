303553da80a14b3dca39018b4ef761b831105205
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2022-12-10 11:02:40 -0600

    Add score lines

--- src/simulation.rs
+++ src/simulation.rs
@@ -16,9 +16,10 @@ pub const WORLD_X_SIZE: f32 = 1000.0;
 pub const WORLD_Y_SIZE: f32 = 560.0;
 pub const FLOOR_HEIGHT: f32 = WORLD_Y_SIZE * 0.1;
 pub const FLOOR_TOP_Y: f32 = WORLD_Y_SIZE - FLOOR_HEIGHT;
+pub const SCORE_PER_SCREEN: i32 = 10; // The score range displayed on the screen (x segments)
 
 const GRAVITY: f32 = 200.0;
-const SCORE_SCALE_FACTOR: f32 = 10.0 / WORLD_X_SIZE;
+const SCORE_SCALE_FACTOR: f32 = SCORE_PER_SCREEN as f32 / WORLD_X_SIZE;
 // Muscle extension and contraction range, where 0.0 is normal, -1.0 is maximum contraction, and 1.0 is double extension
 const MAX_MUSCLE_CONTRACTION: f32 = -0.5;
 const MAX_MUSCLE_EXTENSION: f32 = 0.5;
@@ -235,11 +236,21 @@ impl Simulation {
         Vector2::new((top_left.x + bottom_right.x) / 2.0, top_left.y)
     }
 
+    /// Converts a x position to a score
+    pub fn x_to_score(x: f32) -> f32 {
+        (x - (WORLD_X_SIZE / 2.0)) * SCORE_SCALE_FACTOR
+    }
+
+    /// Converts a score to a x position
+    pub fn score_to_x(score: f32) -> f32 {
+        (score / SCORE_SCALE_FACTOR) + (WORLD_X_SIZE / 2.0)
+    }
+
     /// Gets the score (furthest x distance) of this simulation
     pub fn get_score(&self) -> f32 {
         let (_, bottom_right) = self.get_bounds();
 
-        (bottom_right.x - (MAX_WORLD_X / 2.0)) * SCORE_SCALE_FACTOR
+        Self::x_to_score(bottom_right.x)
     }
 
     /// Steps the muscles one step forward in time
